8f2697065259dcabe78c222b062c1afeb5c5b24a
Liam Bakar <20liamb@students.harker.org>
2020-11-07 16:52:49 -0500

    data anal.

--- DataAnalysis.py
+++ DataAnalysis.py
@@ -10,12 +10,12 @@ from Hackathon import createDictionary
 
 # returns a list of the words with high frequencies
 # assume that the list of frequencies is in order
-def return_frequency_words() -> List(str):
+def return_frequency_words(dictionary: dict) -> List(str):
     length = len(getFrequencies)
     len_highf = length // 3  # assuming list of frequencies is in decreasing order
     hf_words = []
-    for key in createDictionary.range(0, len_highf):
-        hf_words.append(createDictionary[key])
+    for key in dictionary.range(0, len_highf):
+        hf_words.append(dictionary[key])
     return hf_words
 
 # return the sentences containing words from the highest frequency word list
